cd5c4964d61cb6165d0c950fba735d38af121620
moiseselephant00 <telesmoisesmendes@gmail.com>
2025-04-06 21:04:04 -0300

    errodetabela

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Materia
+from .models import Assunto
+from .models import Aula
+from .models import Quiz
 
 # Register your models here.
+admin.site.register(Materia)
+admin.site.register(Assunto)
+admin.site.register(Aula)
+admin.site.register(Quiz)
